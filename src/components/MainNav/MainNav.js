e25d09427b0f9a206acf97780badf107a5089823
triciamedina <tricia.medina@gmail.com>
2019-12-02 17:51:18 -0800

    implement collapsible css menu

--- src/components/MainNav/MainNav.js
+++ src/components/MainNav/MainNav.js
@@ -1,9 +1,25 @@
 import React, { Component } from 'react'
 import Logo from '../Logo/Logo'
 import './MainNav.css'
-import Hamburger from '../Hamburger/Hamburger'
 
 class MainNav extends Component {
+    renderPublicCollapsibleMenu() {
+        return (
+            <div className="PublicCollapsibleMenu">
+                <input type="checkbox" id="menu" />
+                <label htmlFor="menu"></label>
+                <ul className="PublicCollapsibleMenu__list">
+                    <li>
+                        <button className="text-button--small">Sign in</button>
+                    </li>
+                    <li>
+                        <button className="contained-button--large">Get started</button>
+                    </li>
+                </ul>
+            </div>
+        )
+    }
+
     renderPublicMenu() {
         return (
             <div className="PublicMenu">
@@ -22,9 +38,10 @@ class MainNav extends Component {
     render() {
         return (
             <header className="MainNav--sticky">
+                {/* Logo component only shows in Public menu */}
                 <Logo />
                 <nav>
-                    <Hamburger />
+                    {this.renderPublicCollapsibleMenu()}
                     {this.renderPublicMenu()}
                 </nav>
             </header>
