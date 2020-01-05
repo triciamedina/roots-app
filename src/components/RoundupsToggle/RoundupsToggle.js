ce373328ee6c6c5f47d4089d5d1cebef93db5338
triciamedina <tricia.medina@gmail.com>
2020-01-05 14:45:07 -0800

    troubleshoot css display issues for Safari

--- src/components/RoundupsToggle/RoundupsToggle.js
+++ src/components/RoundupsToggle/RoundupsToggle.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import './RoundupsToggle.css'
 import RootsContext from '../../contexts/RootsContext'
+import ReactTooltip from 'react-tooltip'
 
 class RoundupsToggle extends Component {
     static contextType = RootsContext
@@ -9,7 +10,7 @@ class RoundupsToggle extends Component {
             <div className='roundups__toggle-container'>
                 <label htmlFor='roundups-toggle' className='toggle'>
                     <span className='roundups-toggle__title'>
-                        Turn on automatic round ups
+                        Automatic round ups
                     </span>
                     <input 
                         type='checkbox'
@@ -18,8 +19,12 @@ class RoundupsToggle extends Component {
                         onChange={this.context.onAutoRoundupsChange}
                         required 
                     />
-                    <span className='slider--round'></span>
+                    <span className='slider--round'
+                        data-tip='Turn on'
+                    >
+                    </span>
                 </label>
+                <ReactTooltip />
             </div>
         )
     }
