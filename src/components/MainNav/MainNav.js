9d53657881ce1bd93e2349862db05e019aef35aa
triciamedina <tricia.medina@gmail.com>
2020-01-18 14:55:28 -0800

    add aria-label to logged in menu

--- src/components/MainNav/MainNav.js
+++ src/components/MainNav/MainNav.js
@@ -42,7 +42,7 @@ class MainNav extends Component {
             <header className='MainNav--private'>
                 <nav>
                     <div className='CollapsibleMenu'>
-                        <input type='checkbox' id='menu' />
+                        <input type='checkbox' id='menu' aria-label='Menu'/>
                         <label htmlFor='menu'></label>
                         <ul className='CollapsibleMenu__list'>
                             <li>
