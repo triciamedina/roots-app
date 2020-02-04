f9eb3a010136145c785480fd2c80795ec31e9391
triciamedina <tricia.medina@gmail.com>
2020-02-03 19:07:51 -0800

    move tab role to Link components in Tabs.js

--- src/components/Tabs/Tabs.js
+++ src/components/Tabs/Tabs.js
@@ -12,28 +12,32 @@ class Tabs extends Component {
                 className='Tabs' 
                 aria-label='Dashboard tabs'
                 role='tablist' 
-                aria-owns='tab-1 tab-2'
             >
-                <Link to={'/dashboard/roundups'} className='Tabs__container'>
+                <Link 
+                    to={'/dashboard/roundups'}
+                    className='Tabs__container'
+                    role='tab'
+                    aria-selected={active === 'roundups'}
+                    aria-controls='panel-1' 
+                    id='tab-1'
+                    tabIndex={active === 'roundups' ? '0' : '-1'}
+                >
                     <Button 
                         className={'Button--tab ' + (active === 'roundups' && 'active')}
-                        role='tab'
-                        aria-selected={active === 'roundups'}
-                        aria-controls='panel-1' 
-                        id='tab-1'
-                        tabIndex={active === 'roundups' ? '0' : '-1'}
                     >
                         <h2 className='Tab__title'>Round ups</h2>
                     </Button>
                 </Link>
-                <Link to={'/dashboard/donations'}>
+                <Link 
+                    to={'/dashboard/donations'}
+                    role='tab'
+                    aria-selected={active === 'donations'}
+                    aria-controls='panel-2' 
+                    id='tab-2'
+                    tabIndex={active === 'donations' ? '0' : '-1'}
+                >
                     <Button 
                         className={'Button--tab ' + (active === 'donations' && 'active')}
-                        role='tab'
-                        aria-selected={active === 'donations'}
-                        aria-controls='panel-2' 
-                        id='tab-2'
-                        tabIndex={active === 'donations' ? '0' : '-1'}
                     >
                         <h2 className='Tab__title'>Donation history</h2>
                     </Button>
